b73ccc5ba7a3327bfd4aedbac8011d55e6332382
chetan_vadingekar <vadingekarchetan@gmail.com>
2023-06-29 16:04:43 +0530

    save

--- gcd.py
+++ gcd.py
@@ -4,11 +4,12 @@ def compute_gcd(num1,num2):
         minNum = num1
     else:
         minNum =  num2
-    
+    # find out gcd number
     for i in range(1,minNum + 1):
         if (num1 % i == 0) and (num2 % i == 0):
             minNum = i
-
+    
+    # printing the gcd number of num1 and num2
     print(f"The GCD of {num1} and {num2} is : {minNum}")
 
 compute_gcd(12,17)
